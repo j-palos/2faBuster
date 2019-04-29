bf5c42d1680ce8d9de2243ce5a17e1ad57c2cd13
Ozone Kafley <kafleyozone@gmail.com>
2019-04-29 18:05:20 -0500

    can successfully check if login creds are invalid or we're at the 2fa screen

--- selenium_driver.py
+++ selenium_driver.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.common.exceptions import NoSuchElementException
 
 import os
 import sys
@@ -10,14 +11,13 @@ import time
 ADDRESS = 'http://127.0.0.1:8000/'
 
 
-# Note: firefox (gecko) driver must be in PATH for this to work
-# htmlfile, **credentials
-def do_login(htmlfile, **credentials):
+# Note: firefox (gecko) driver must be in system PATH for this to work
+def do_login(**credentials):
     options = webdriver.FirefoxOptions()
     # options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)
 
-    # Visit this webpage.
+    # Visit the webpage.
     driver.get("https://reddit.com/login")
 
     # Send credentials
@@ -32,13 +32,34 @@ def do_login(htmlfile, **credentials):
     time.sleep(3)
 
     # Was the login successful? If so, is the 2FA page showing up?
-    print(submit_btn.get_attribute("innerHTML"))
+    error_found = False
+    twoauth_found = False
+    while not error_found and not twoauth_found:
+        time.sleep(0.3)
+        length_error = len(driver.find_elements_by_class_name('m-error'))
+        print("length_error" + str(length_error))
+        length_twofa = len(driver.find_elements_by_class_name('mode-2fa'))
+        print("length_twofa" + str(length_twofa))
+        if length_error > 0:
+            error_found = True
+        if length_twofa > 0:
+            twoauth_found = True
+
+
+    if error_found:
+        print("an error was found")
+    elif twoauth_found:
+        print("got to twoauth field")
+    # try:
+    #     driver.find_element_by_class_name('m-error')
+    #     print("! ! ERROR ! !")
+    # except NoSuchElementException:
+    #     print("no error icon after 3 seconds")
+    # print(submit_btn.get_attribute("innerHTML"))
 
     
 
 def go():
-    # do_login("twofabuster.html", Username="pepe", Password="password")
-    # do_login("authtoken.html", Password=1234) 
-    do_login("login-orig.html", Username="2fabusters", Password="attackatdawn")
+    do_login(Username="2fabusters", Password="attackatdawn", Code="000000")
 
 go()
